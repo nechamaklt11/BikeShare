516d1e9074d04843528f93fb375e3c05b921c1db
nechamaklt11 <48054996+nechamaklt11@users.noreply.github.com>
2020-08-02 02:26:51 +0300

    Delete main.m

--- main.m
+++
@@ -1,9 +0,0 @@
-%clear all; clc; close all;
-
-bike_data = data_prep('day.csv') %read data and create training and test groups
-
-visualize_data(bike_data) 
-%%
-nn_params=set_nn_params;
-neural_network(inputs,targets,train_ind,valid_ind,nn_params);
-
