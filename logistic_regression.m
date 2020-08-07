b914ddf68b2d9b32b377659d92a4e8f7637635e2
nechamaklt11 <48054996+nechamaklt11@users.noreply.github.com>
2020-08-07 14:28:14 +0300

    Add files via upload

--- logistic_regression.m
+++ logistic_regression.m
@@ -1,56 +1,69 @@
-function weights = logistic_regression(x,t,train_ind,valid_ind,params)
+function [accuracy,accuracy_avg] = logistic_regression(x,t,train_idx,valid_idx,params,test_inputs,test_targets)
 %EVERY COLUMN REPRESENTS A SAMPLE!!!
 %params: max_epoch, threshold, learning_rate
 
 lr = params.learning_rate;
 max_epoch = params.max_epochs;
 valid_errors = params.max_fail;
-train_size = length(train_ind);
+lambda = params.lambda;
+train_size = length(train_idx{1});
 
 [num_features,num_samples]=size(x);
-w =rand(num_features+1,1); %add one for bias
 bias = ones(1,num_samples); %add bias after the last feature
 x = [x ; bias]; %add bias row
+accuracy = zeros(1,5);
+weights=zeros(num_features+1,5);
 
-rng(1); %for better randomization
+rng(1) %for better randomization
 
-%STOCHASTIC GRADIENT DESCENT
-cost=zeros(1,max_epoch);
-valid_cost = zeros(1,max_epoch);
-epoch=1;
-bad_valid = 0; %for early stop
-best_w = w;
-
-while epoch<max_epoch && bad_valid<valid_errors
-    train_ind = randperm(train_size);
-    for i=1:train_size
-        sample_ind = train_ind(i);
-        sample = x(:,sample_ind);
-        target = t(sample_ind);
-        wx = dot(sample,w); %multipluting the weights vector and the sample
-        g = logistic_func(wx);
-        w = w - lr*((g-target).*sample); %update weights
-        cost(epoch)=cost(epoch)+cross_entropy(g,target);
+for k=1:5
+    w =rand(num_features+1,1); %add one for bias
+    train_ind=train_idx{k};
+    valid_ind=valid_idx{k};
+    %STOCHASTIC GRADIENT DESCENT
+    cost=zeros(1,max_epoch);
+    valid_cost = zeros(1,max_epoch);
+    epoch=1;
+    bad_valid = 0; %for early stop
+    best_w = w;
+    
+    while epoch<max_epoch && bad_valid<valid_errors
+        Ind = randperm(train_size);
+        for i=1:train_size
+            sample_ind = train_ind(Ind(i));
+            sample = x(:,sample_ind);
+            target = t(sample_ind);
+            wx = dot(sample,w); %multipluting the weights vector and the sample
+            g = logistic_func(wx);
+            w = w - lr*((g-target).*sample+(2*lambda).*w); %update weights
+            cost(epoch)=cost(epoch)+cross_entropy(g,target)+lambda*sum(w.^2);
+        end
+        cost(epoch)=cost(epoch)/train_size;
+        %check validation
+        X = x(:,valid_ind);
+        T = t(valid_ind);
+        valid_cost(epoch)= calc_valid_cost(X,T,w,lambda);
+        if epoch>2 && valid_cost(epoch)>valid_cost(epoch-1)
+            bad_valid = bad_valid+1;
+        else
+            bad_valid = 0;
+            best_w=w;
+        end
+        epoch = epoch+1;
     end
-    cost(epoch)=cost(epoch)/train_size;
-    %check validation
-    X = x(:,valid_ind);
-    T = t(valid_ind);
-    valid_cost(epoch)= calc_valid_cost(X,T,w);
-    if epoch>2 && valid_cost(epoch)>valid_cost(epoch-1)
-        bad_valid = bad_valid+1;
-    else
-        bad_valid = 0;
-        best_w=w;
-    end
-    epoch = epoch+1;
+    
+    cost = cost(1:epoch-1);
+    valid_cost = valid_cost(1:epoch-1);
+    weights(:,k) = best_w;
+    
+    %test accuracy
+    X = [test_inputs; ones(1,size(test_inputs,2))]; %add a bias row
+    accuracy(k) = test_regression(X,test_targets,best_w);
+    plot_perform(cost,valid_cost,epoch-1)
 end
-
-cost = cost(1:epoch-1);
-valid_cost = valid_cost(1:epoch-1);
-
-weights = best_w;
-plot_perform(cost,valid_cost,epoch-1)
+%average performance
+avg_weights = mean(weights,2);
+accuracy_avg=test_regression(X,test_targets,avg_weights);
 
 function g = logistic_func(x)
 % logistic function
@@ -61,11 +74,11 @@ function j = cross_entropy(g,t)
 j=t.*log(g)+(1-t).*log(1-g);
 j=-j;
 
-function valid_cost = calc_valid_cost(x,t,w)
+function valid_cost = calc_valid_cost(x,t,w,lambda)
 %compute validation cost
 y = w'*x;
 g = logistic_func(y);
-valid_cost=sum(cross_entropy(g,t))/length(t);
+valid_cost=sum(cross_entropy(g,t)+lambda*sum(w.^2))/length(t);
 
 function plot_perform(cost,valid_cost,epoch)
 %plot performance - training error vs validation error
--- neural_network.m
+++ neural_network.m
@@ -1,29 +1,43 @@
 
-function neural_network(x,t,train_ind,valid_ind,params) 
+function [accuracy,accuracy_avg] = neural_network(x,t,train_ind,valid_ind,params,test_inputs,test_targets)
 %applying a neural network on input x and target t.
 %train_ind,valid_ind: indeces for validation and train groups
 %params: a structure with default setting
 
-rng(1) %for better randomization
+rng(2) %for better randomization
 
-%CREATE NETWORK
-net = feedforwardnet(params.hidden_layers,'traingd');  %gradient decent training 
-net.numInputs=1; %we have one input with a few parameters
+predicted_y=zeros(5,length(test_targets));
+accuracy=zeros(1,5);
+for i=1:5
+    %CREATE NETWORK
+    net = feedforwardnet(params.hidden_layers,'traingd');  %gradient decent training
+    net.numInputs=1; %we have one input with a few parameters
+    
+    % SET PARAMETERS & HYPERPARAMETERS
+    net.layers{1}.transferFcn='logsig'; %training function is sigmoid
+    net.layers{2}.transferFcn='tansig'; %output layer. can be either tansig or pure linear
+    net.performFcn ='crossentropy'; %matching error function - X entropy
+    net.trainParam.epochs = params.max_epochs;
+    net.trainParam.lr = params.learning_rate;
+    net.trainParam.max_fail = params.max_fail; %early stop condition
+    net.performParam.regularization =params.lambda;
+    net.divideFcn = 'divideind'; %divide train and validation groups manually
+    net.trainParam.showWindow = false;
+    net.divideParam.trainInd =train_ind{i}; net.divideParam.valInd  = valid_ind{i}; net.divideParam.testInd  = []; %use default validation and training indeces
 
-% SET PARAMETERS & HYPERPARAMETERS
-net.layers{1}.transferFcn='logsig'; %training function is sigmoid
-net.layers{2}.transferFcn='tansig'; %output layer. can be either tansig or pure linear
-net.performFcn ='crossentropy'; %matching error function - X entropy
-net.trainParam.epochs = params.max_epochs; 
-net.trainParam.lr = params.learning_rate;
-net.trainParam.max_fail = params.max_fail; %early stop condition 
-net.performParam.regularization =params.lambda;
-net.divideFcn = 'divideind'; %divide train and validation groups manually
-net.divideParam.trainInd =train_ind; net.divideParam.valInd  = valid_ind; net.divideParam.testInd  = []; %use default validation and training indeces
-net.trainParam.showWindow = false;
-
-%TRAIN NETWTORK AND COMPARE TO VALIDATION GROUP
-[net,tr]=train(net,x,t); %training the network
-plotperform(tr) %plotting performance graph
-view(net) %visualize network activity
+    %TRAIN NETWTORK AND COMPARE TO VALIDATION GROUP
+    [net,tr]=train(net,x,t); %training the network
+    plotperform(tr) %plotting performance graph
+    predicted_y(i,:)=net(test_inputs);
+    accuracy(i)= evaluate_net(test_targets,predicted_y(i,:));
 end
+predicted_y_avg=mean(predicted_y,1);
+accuracy_avg=evaluate_net(test_targets,predicted_y_avg);
+view(net)
+
+function accuracy = evaluate_net(targets,predictions)
+predictions(predictions<0.5)=0; predictions(predictions>=0.5)=1;
+diff = targets-predictions;
+num_hits= sum(diff==0);
+accuracy = (num_hits/length(targets))*100;
+
