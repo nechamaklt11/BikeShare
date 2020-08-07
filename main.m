c9827ce205538909018b8a67a7db53e564734b10
polinam94 <54074869+polinam94@users.noreply.github.com>
2020-08-07 10:42:11 +0300

    Add files via upload

--- main.m
+++ main.m
@@ -3,11 +3,11 @@ clear; clc; close all;
 %% DATA PROCESSING
 bike_data = data_prep('day.csv'); %read data and create training and test groups
 visualize_data(bike_data) 
-load('data_for_analysis') %every column is a sample!!!!!!!!!!!!!!!
+load('data_for_analysis.mat') %every column is a sample!!!!!!!!!!!!!!!
 
 %% NEURAL NETWORK
 nn_params=set_nn_params;
-net =neural_network(inputs,targets,train_ind,valid_ind,nn_params);
+net=neural_network(inputs,targets,train_ind,valid_ind,nn_params);
 view(net);
 predicted_y=net(test_inputs);
 net_accuracy = evaluate_net(test_targets,predicted_y);
@@ -17,3 +17,6 @@ reg_params = set_reg_params;
 weights = logistic_regression(inputs,targets,train_ind,valid_ind,reg_params);
 X = [test_inputs; ones(1,size(test_inputs,2))]; %add a bias row
 accuracy = test_regression(X,test_targets,weights);
+
+%% SVM model 
+SVMtrain(inputs ,targets, nn_params);
