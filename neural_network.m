6d53084a341cda269b0404d0f963f7809b30fa8d
nechamaklt11 <48054996+nechamaklt11@users.noreply.github.com>
2020-08-26 21:20:15 +0300

    Add files via upload

--- neural_network.m
+++ neural_network.m
@@ -1,18 +1,22 @@
 
-function [accuracy,accuracy_avg] = neural_network(x,t,train_ind,valid_ind,params,test_inputs,test_targets)
+function [accuracy,accuracy_avg,predicted_y_avg,vperf] = neural_network(data,params)
 %applying a neural network on input x and target t.
-%train_ind,valid_ind: indeces for validation and train groups
 %params: a structure with default setting
+%vperf: average validation performance
+load(data)
+x=inputs;
+t=targets;
+[train_ind,valid_ind]=cross_validation(length(targets),5); %train_ind,valid_ind: indeces for validation and train groups
 
 rng(2) %for better randomization
 
 predicted_y=zeros(5,length(test_targets));
 accuracy=zeros(1,5);
+best_vperf=zeros(1,5);
 for i=1:5
     %CREATE NETWORK
     net = feedforwardnet(params.hidden_layers,'traingd');  %gradient decent training
     net.numInputs=1; %we have one input with a few parameters
-    
     % SET PARAMETERS & HYPERPARAMETERS
     net.layers{1}.transferFcn='logsig'; %training function is sigmoid
     net.layers{2}.transferFcn='tansig'; %output layer. can be either tansig or pure linear
@@ -24,16 +28,20 @@ for i=1:5
     net.divideFcn = 'divideind'; %divide train and validation groups manually
     net.trainParam.showWindow = false;
     net.divideParam.trainInd =train_ind{i}; net.divideParam.valInd  = valid_ind{i}; net.divideParam.testInd  = []; %use default validation and training indeces
-
+    
     %TRAIN NETWTORK AND COMPARE TO VALIDATION GROUP
     [net,tr]=train(net,x,t); %training the network
-    plotperform(tr) %plotting performance graph
+    if params.plot==true
+        plotperform(tr) %plotting performance graph
+        view(net)
+    end
     predicted_y(i,:)=net(test_inputs);
     accuracy(i)= evaluate_net(test_targets,predicted_y(i,:));
+    best_vperf(i) = tr.best_vperf;
 end
 predicted_y_avg=mean(predicted_y,1);
 accuracy_avg=evaluate_net(test_targets,predicted_y_avg);
-view(net)
+vperf = mean(best_vperf);
 
 function accuracy = evaluate_net(targets,predictions)
 predictions(predictions<0.5)=0; predictions(predictions>=0.5)=1;
--- tree.m
+++ tree.m
@@ -1,34 +1,83 @@
-function output = tree(inputs ,targets, test_inputs, test_targets)
+function [tree_acc, tree_acc_avg, predictions_avg, tree_vperf] = tree(inputs ,targets, test_inputs, test_targets, data)
+
+load(data)
+x=inputs';
+t=targets';
+[train_idx,valid_idx]=cross_validation(length(targets),5); %train_ind,valid_ind: indeces for validation and train groups
+train_size = length(train_idx{1});
+
+A{1,1}=x([1:117],:);
+A{1,2}=x([118:234],:);
+A{1,3}=x([234:350],:);
+A{1,4}=x([351:467],:);
+A{1,5}=x([469:585],:);
+B{1,1}=t([1:117]);
+B{1,2}=t([118:234]);
+B{1,3}=t([234:350]);
+B{1,4}=t([351:467],:);
+B{1,5}=t([469:585],:);
 
 inputsT=inputs'; targetsT=targets'; tInputs=test_inputs'; tTargets=test_targets';
 
-t=fitctree(inputsT, targetsT, ...
-    'OptimizeHyperparameters','all', ...
-    'HyperparameterOptimizationOptions',struct('KFold', 5, 'SaveIntermediateResults', true, 'MaxObjectiveEvaluations', 20), ...
-    'PredictorNames', {'date', 'season', 'year', 'month', 'holiday', 'weekday', 'working day', 'situation', 'temp', 'atemp', 'humidity', 'windspeed'});
+weights=ones(117,1);
+labelout=[];
 
-resubcost = resubLoss(t ,'Subtrees','all');
-[cost,secost,ntermnodes,bestlevel] = cvloss(t,'Subtrees','all');
-figure(gcf);
-plot(ntermnodes,cost,'b-', ntermnodes,resubcost,'r--')
-xlabel('Number of terminal nodes');
-ylabel('Cost (misclassification error)')
-[mincost,minloc] = min(cost);
-cutoff = mincost + secost(minloc);
-hold on
-plot([0 20], [cutoff cutoff], 'k:')
-plot(ntermnodes(bestlevel+1), cost(bestlevel+1), 'mo')
-legend('Cross-validation','Resubstitution','Min + 1 std. err.','Best choice')
-
-pt = prune(t,'Level',bestlevel);
-view(pt,'Mode','graph')
-
-[labelout, score]=predict(t, tInputs);
-
-accuracy = 0;
-for i=1:length(tTargets)
-    if labelout(i)==tTargets(i)
-        accuracy=accuracy+1;
+for k=1:5
+    tree=fitctree(A{1,k}, B{1,k}, ...
+        'MinLeafSize',2, 'MaxNumSplits', 50, 'Prune', 'on', 'Weights', weights,...
+        'PredictorNames', {'date', 'season', 'year', 'month', 'holiday', 'weekday', 'working day', 'situation', 'temp', 'atemp', 'humidity', 'windspeed'});
+    
+    [cost,secost,ntermnodes,bestlevel] = cvloss(tree, 'Subtrees','all');
+    resubcost = resubLoss(tree, 'Subtrees','all');
+    plot_perform(ntermnodes, cost, resubcost, secost, bestlevel);
+    
+    labelout(:,k)=predict(tree, tInputs);
+    accuracy=0;
+    for i=1:length(tTargets)
+        if labelout(i,k)==tTargets(i)
+            accuracy=accuracy+1;
+        end
     end
+    fold_acc(k)=accuracy/length(tTargets)*100;
+    tree_vperf(k)=min(cost);
+    
 end
-accuracy=accuracy/length(tTargets)*100;
+%'MinLeafSize',2, 'MaxNumSplits', Inf, 'Prune', 'off', 'Weights', weights,...
+tree_vperf=mean(tree_vperf);
+pt = prune(tree,'Level',bestlevel);
+view(pt,'Mode','graph');
+
+%average cross validation performance
+predictions_avg = round(mean(labelout'));
+diff=predictions_avg-tTargets';
+num_hits= sum(diff==0);
+tree_acc_avg = (num_hits/length(test_targets))*100;
+
+for k=1:5
+    accuracy=0;
+    for i=1:length(tTargets)
+        if labelout(i,k)==tTargets(i)
+            accuracy=accuracy+1;
+        end
+    end
+    tree_acc(k)=accuracy/length(tTargets)*100;
+end
+tree_acc_avg=sum(tree_acc)/k;
+
+n=1;
+function plot_perform(ntermnodes, cost, resubcost, secost, bestlevel)
+%plot performance - training error vs validation error
+    figure;
+    plot(ntermnodes,cost,'b-', ntermnodes,resubcost,'r--')
+    xlabel('Number of terminal nodes');
+    ylabel('Cost (misclassification error)')
+    [mincost,minloc] = min(cost);
+    cutoff = mincost + secost(minloc);
+    hold on
+    plot([0 ntermnodes(1)], [cutoff cutoff], 'k:')
+    plot(ntermnodes(bestlevel+1), cost(bestlevel+1), 'mo')
+    legend('Cross-validation','Resubstitution','Min + 1 std. err.','Best choice')
+    hold off
+end
+
+end
