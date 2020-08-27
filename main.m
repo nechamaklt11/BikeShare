11a794f4dc51e844180b2ac9b4fc135f19e5d4ef
polinam94 <54074869+polinam94@users.noreply.github.com>
2020-08-27 09:17:47 +0300

    Add files via upload

--- main.m
+++ main.m
@@ -1,33 +1,34 @@
 clear; clc; close all;
-
 %% DATA PROCESSING
 bike_data_edited = data_prep('day.csv'); %read data and create training and test groups
 visualize_data(bike_data_edited)
-load('data4analysis')  %every column is a sample!!
+load('data4analysis.mat')  %every column is a sample!!
 [train_ind,valid_ind]=cross_validation(length(targets),5); 
  
 %% NEURAL NETWORK
 nn_params=set_nn_params;
 [net_accuracy,net_accuracy_avg,predicted_net_y,net_vperf] =neural_network('data4analysis',nn_params);
-accuracy_bar(net_accuracy, net_accuracy_avg, 'net accuracy across folds')
+accuracy_bar(net_accuracy, net_accuracy_avg, 'net accuracy across folds');
 
 %% LOGISTIC REGRESSION 
 reg_params = set_reg_params;
 [reg_accuracy,reg_accuracy_avg,predicted_reg_y,reg_vperf] = logistic_regression('data4analysis',reg_params);
-accuracy_bar(reg_accuracy, reg_accuracy_avg, 'logistic regression accuracy across folds')
+accuracy_bar(reg_accuracy, reg_accuracy_avg, 'logistic regression accuracy across folds');
+
+%% Tree model 
+tree_params = set_tree_params;
+[tree_acc, tree_acc_avg, predicted_tree_y, tree_vperf]=tree(inputs, targets, test_inputs, test_targets, tree_params);
+accuracy_bar(tree_acc, tree_acc_avg, 'trees accuracy across folds')
+% !!!! Change data load
 
 %% SVM model 
 SVMTrain_update(inputs ,targets, test_inputs, test_targets);
-
-%% Tree model 
-
-[tree_acc, tree_acc_avg, predicted_tree_y, tree_vperf]=tree(inputs ,targets, test_inputs, test_targets, 'data4analysis');
 
 %%
 net_best_params=grid_search('learning_rate',[0 0.001 0.01 0.1 1],'max_epochs',[1 10 100 200 1000],'hidden_layers',[2 1 3 5 10],'data4analysis',nn_params,1);
 %%
 reg_best_params=grid_search('learning_rate',[0.001 0.01 0.1 0.5 1],'max_epochs',[10 20 100 250,500],'lambda',[0 0.005 0.05 0.5],'data4analysis',reg_params,2);
-
-%logistic_best_params=grid_search()
-
-
+%%
+tree_best_params=grid_search('learning_rate',[0 0.001 0.01 0.1 1],'max_epochs',[1 10 100 200 1000],'hidden_layers',[2 1 3 5 10],'data4analysis',nn_params,3);
+%%
+SVM_best_params=grid_search('learning_rate',[0 0.001 0.01 0.1 1],'max_epochs',[1 10 100 200 1000],'hidden_layers',[2 1 3 5 10],'data4analysis',nn_params,4);
