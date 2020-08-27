8cad1cb6b7e6d21f10a0c6a41aaa44b176178099
polinam94 <54074869+polinam94@users.noreply.github.com>
2020-08-27 09:31:39 +0300

    Add files via upload

--- set_tree_params.m
+++ set_tree_params.m
@@ -1,2 +1,2 @@
 function params = set_tree_params()
-params=struct('MinLeafSize',2, 'MaxNumSplits', 50, 'Prune', false, 'lambda',0, 'plot',false);
+params=struct('MinLeafSize',2, 'MaxNumSplits', 50, 'Prune', false, 'plot',false);
