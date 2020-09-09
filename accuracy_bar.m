8863bf946119a596ded71209747fb6e4c6bf5bdc
nechamaklt11 <48054996+nechamaklt11@users.noreply.github.com>
2020-09-09 13:20:31 +0300

    Update accuracy_bar.m

--- accuracy_bar.m
+++ accuracy_bar.m
@@ -2,7 +2,7 @@ function accuracy_bar(acc, avg_acc, bar_title)
 figure
 bar(acc,'m')
 xlabel('fold'); ylabel('accurecy (%)')
-ylim([70 90]);
+ylim([70 100]);
 hold on
 line([0.5 5.5],[mean(acc) mean(acc)],'Color','k','LineWidth',1)
 line([0.5 5.5],[avg_acc, avg_acc],'Color','b','LineWidth',1)
