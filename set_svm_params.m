6ea9621810b90a725b6e54b3d270af809d630e8e
nechamaklt11 <48054996+nechamaklt11@users.noreply.github.com>
2020-09-09 13:23:47 +0300

    Update set_svm_params.m

--- set_svm_params.m
+++ set_svm_params.m
@@ -1,2 +1,2 @@
 function params = set_svm_params()
-params=struct('PassLimit', 20, 'LearnRate', 0.05, 'Lambda', 0.05, 'plot',true);
+params=struct('PassLimit', 20, 'LearnRate', 0.05, 'Lambda', 0.05, 'plot',false);
