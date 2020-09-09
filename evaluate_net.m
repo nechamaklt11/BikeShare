976b375240c6376ea3548a335d2366d04dc13498
nechamaklt11 <48054996+nechamaklt11@users.noreply.github.com>
2020-09-09 13:09:20 +0300

    Delete evaluate_net.m

--- evaluate_net.m
+++
@@ -1,7 +0,0 @@
-function accuracy = evaluate_net(targets,predictions)
-predictions(predictions<0.5)=0; predictions(predictions>=0.5)=1;
-diff = targets-predictions;
-num_hits= sum(diff==0);
-accuracy = (num_hits/length(targets))*100;
-
-
